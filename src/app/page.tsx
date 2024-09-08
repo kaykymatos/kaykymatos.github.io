8c3e2725f54368c41adef21909bcb11ca3b66568
Kayky Matos Santana <kayky7277@gmail.com>
2024-09-08 10:45:27 -0300

    responsividade

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,27 @@
-import ContatoSection from './sections/contato'
-import ExperienciaSection from './sections/experiencia'
-import FormacaoSection from './sections/formacao'
-import HomeSetion from './sections/home'
-import InteressesSection from './sections/interesses'
-import ProjetosSection from './sections/projetos'
-import SkillsSection from './sections/skills'
-import SobreSection from './sections/sobre'
+import ContatoSection from "./sections/contato";
+import ExperienciaSection from "./sections/experiencia";
+import FormacaoSection from "./sections/formacao";
+import HomeSetion from "./sections/home";
+import HowAmI from "./sections/howAmI";
+import ProjetosSection from "./sections/projetos";
+import SkillsSection from "./sections/skills";
 
 export default function Home() {
   return (
-  <>
-  <header className="py-5">
-  <div className="container px-5 padding-page pb-5"></div>
-  
-  <HomeSetion/>
-    <ExperienciaSection/>
-    
-    <ProjetosSection/>
+    <>
+      <header className="py-5">
+        <div className="container px-5 padding-page pb-5"></div>
 
-    <FormacaoSection/>
-    <SkillsSection/>
-    </header>
-    <ContatoSection/>
+        <HomeSetion />
+        <HowAmI />
+        <ExperienciaSection />
+
+        <ProjetosSection />
+
+        <FormacaoSection />
+        <SkillsSection />
+      </header>
+      <ContatoSection />
     </>
-
-  )
+  );
 }
